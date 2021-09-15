4ccbda379815522c439c688ae14f6eed575125da
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-15 22:16:48 +0530

    player

---
+++ player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_ladder
+{
+    class player 
+ {
+       public string name;
+       public int currentposition;
+       public int previousposition;
+       
+  
+  public player()		// constructor implementation. 
+{
+            name = "";
+  currentposition = 1;		// initialization of variables.
+  previousposition= 1;
+
+}
+        
+
+public int Rolling_dice()        // Rollingdie function.
+{
+            Random random = new Random();
+            return random.Next(1,7);  // using Random function.
+}
+
+
+}
+}
